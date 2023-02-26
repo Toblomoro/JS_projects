71b0d5304bfc3a3fcaf50cc8eb13bfcc805db868
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2023-02-26 11:22:18 -0600

    Comleted modal tutorial

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -2,3 +2,21 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const toggleBtn = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal-overlay");
+
+toggleBtn.addEventListener("click", function(){
+ console.log(modal.classList);
+//  if(sidebar.classList.contains("show-sidebar")){
+//     sidebar.classList.remove('show-sidebar')
+//  }else{
+//     sidebar.classList.add('show-sidebar')
+//  }
+modal.classList.toggle('open-modal')
+})
+closeBtn.addEventListener('click', function(){
+
+    modal.classList.remove("open-modal")
+})
