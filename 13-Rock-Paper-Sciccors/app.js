93f1fb83f792f8310827adfbd3b34453a8b3bb85
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2023-06-27 15:05:36 -0500

    started js

--- 13-Rock-Paper-Sciccors/app.js
+++ 13-Rock-Paper-Sciccors/app.js
@@ -1,3 +1,10 @@
+
 const rock = document.querySelector("#r");
 const scissors = document.querySelector("#s");
 const paper = document.querySelector("#p");
+
+const userScore = 0;
+const computerScore = 0;
+
+const userScore_span= document.getElementById("user-score");
+const computerScoreScore_span= document.getElementById("computer-score");
