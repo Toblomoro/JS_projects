40c6f9e6dfaaeb47732f039eba41b2bec217ae68
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2023-06-28 09:36:01 -0500

    DOM cached and working on formulas

--- 13-Rock-Paper-Sciccors/app.js
+++ 13-Rock-Paper-Sciccors/app.js
@@ -8,3 +8,42 @@ const computerScore = 0;
 
 const userScore_span= document.getElementById("user-score");
 const computerScoreScore_span= document.getElementById("computer-score");
+
+const scoreboard_div= document.querySelector(".scoreboard")
+const result_div = document.querySelector(".result")
+
+const rock_div = document.getElementById("r")
+const paper_div = document.getElementById("p")
+const scissors_div = document.getElementById("s")
+
+function getComputerChoice() {
+    const choices = ["r", "p", "s"];
+    const randomNumber = (Math.floor(Math.random() * 3))
+    return choices[randomNumber];
+}
+
+
+
+function game(userChoice) {
+    const computerChoice= getComputerChoice();
+    console.log(computerChoice);
+    console.log("User choice = " + userChoice)
+
+}
+
+function main() {
+    
+rock_div.addEventListener('click', function () {
+        game("r"); 
+    })
+
+paper_div.addEventListener('click', function(){
+    game("p")
+})
+
+scissors_div.addEventListener('click', function(){
+    game("s")
+})
+
+}
+main()
