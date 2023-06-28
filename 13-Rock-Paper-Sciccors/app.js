38bb4621d939589e96a48d1262c4b712ff7476df
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2023-06-28 10:42:47 -0500

    finished

--- 13-Rock-Paper-Sciccors/app.js
+++ 13-Rock-Paper-Sciccors/app.js
@@ -3,14 +3,14 @@ const rock = document.querySelector("#r");
 const scissors = document.querySelector("#s");
 const paper = document.querySelector("#p");
 
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 
-const userScore_span= document.getElementById("user-score");
-const computerScoreScore_span= document.getElementById("computer-score");
+let userScore_span= document.getElementById("user-score");
+let computerScoreScore_span= document.getElementById("computer-score");
 
 const scoreboard_div= document.querySelector(".scoreboard")
-const result_div = document.querySelector(".result")
+let result_div = document.querySelector(".result > p")
 
 const rock_div = document.getElementById("r")
 const paper_div = document.getElementById("p")
@@ -21,13 +21,60 @@ function getComputerChoice() {
     const randomNumber = (Math.floor(Math.random() * 3))
     return choices[randomNumber];
 }
+function converter(letter) {
+    if (letter == "r") return "Rock";
+    if (letter == "p") return "Paper"
+    else return "Scissors"
+    
+}
+function win(userChoice, computerChoice){
+userScore++;
+userScore_span.innerHTML = userScore;
+computerScoreScore_span.innerHTML = computerScore;
+result_div.innerHTML= converter(userChoice) + " beats " + converter(computerChoice) + " You win! 🔥 👍";
+
+    }
+function lose(userChoice, computerChoice) {
+    computerScore++
+    userScore_span.innerHTML = userScore;
+    computerScoreScore_span.innerHTML = computerScore;
+    result_div.innerHTML= converter(userChoice) + " loses to " + converter(computerChoice) + " You lose.... 💩 👎" 
+
+}
+function draw() {
+
+userScore_span.innerHTML = userScore;
+computerScoreScore_span.innerHTML = computerScore;
+result_div.innerHTML="Tie!"   
+}
 
 
 
 function game(userChoice) {
     const computerChoice= getComputerChoice();
     console.log(computerChoice);
-    console.log("User choice = " + userChoice)
+    console.log("User choice = " + userChoice);
+    switch (userChoice + computerChoice) {
+      case "rs":
+      case "sp":
+      case "pr":
+        win(userChoice, computerChoice)
+    
+        break;
+      case "sr":
+      case "ps":
+      case "rp":
+        lose(userChoice, computerChoice);
+        break;
+      case "rr":
+      case "ss":
+      case "pp":
+        draw();
+        break;
+
+      default:
+        break;
+    }
 
 }
 
